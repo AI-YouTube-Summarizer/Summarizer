c5bceacf0cec225d31024a9979dfdaf48d6329a9
Jay Mevada <139486309+deltapheonix-zei@users.noreply.github.com>
2024-12-24 18:12:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ if st.session_state.accepted_terms:
         # Handle YouTube link and fetch video ID
         if youtube_link:
             response = requests.get(youtube_link, headers={
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"})
             soup = BeautifulSoup(response.text, "html.parser")
 
             # Extract and clean the video title
